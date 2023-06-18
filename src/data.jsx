1adbf95a4aa88f3870da1df84b9664c86007f144
jparrker <102701917+jparrker@users.noreply.github.com>
2023-06-18 14:55:44 -0400

    first commit

---
+++ src/data.jsx
@@ -0,0 +1,41 @@
+import { FaBehance, FaFacebook, FaLinkedin, FaTwitter } from "react-icons/fa";
+export const links = [
+  {
+    id: 1,
+    url: "/",
+    text: "home",
+  },
+  {
+    id: 2,
+    url: "/about",
+    text: "about",
+  },
+  {
+    id: 3,
+    url: "/Charts",
+    text: "Charts",
+  },
+  {
+    id: 4,
+    url: "/Data",
+    text: "Data",
+  },
+];
+
+export const social = [
+  {
+    id: 1,
+    url: "https://www.twitter.com",
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    url: "https://www.twitter.com",
+    icon: <FaTwitter />,
+  },
+  {
+    id: 3,
+    url: "https://www.twitter.com",
+    icon: <FaLinkedin />,
+  },
+];
